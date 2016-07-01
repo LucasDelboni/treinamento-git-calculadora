17ad9ab410e934a152551c85b996a7d4a6d1c314
Lucas Delboni <lucas_cd460@hotmail.com>
2016-07-01 19:59:34 +0000

    soma e multiplicacao

---
+++ SomaeMultiplica.java
@@ -0,0 +1,14 @@
+Class SomaeMultiplica {
+
+    public void soma(double x; double y){
+        double resultado = x+y;
+        System.out.println(resultado);
+        
+    }
+
+    public void multiplicaçao (double x; double y) {
+        double resultado = x*y;
+        System.out.println(resultado);
+        
+    }
+}
