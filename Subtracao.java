fe626e47f5c1505f3685551fc83891a8b4652b8d
fernandochiu <fernando.hsieh@usp.br>
2016-07-01 16:51:47 -0300

    escrevi dois metodos que fazem a subtracao e a divisao

---
+++ Subtracao.java
@@ -0,0 +1,17 @@
+import java.*;
+class Subtracao{
+
+public static void subtracao(double a, double b){
+	double resutado= a-b;
+	System.out.println("O resultado eh: "+ resultado);
+}
+
+}
+public static double divisao(double a, double b){
+	double resutado= a/b;
+	System.out.println("O resultado eh: "+ resultado);
+}
+	
+}
+
+
